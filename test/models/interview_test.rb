5195b5861e97c46eca6fed350cbd2c8a5360cc7b
Caleb Rutan <caleb.rutan@thomsonreuters.com>
2017-02-06 16:49:35 -0500

    fixtures and an example test

--- test/models/interview_test.rb
+++ test/models/interview_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class InterviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    create_answers_and_ratings
+  end
+
+  test "interviews have multiple questions" do
+    i = interviews(:first_driver_interview)
+    assert i.interview_questions.any?
+  end
+  
+  test "interview questions should have answers" do
+    i = interviews(:first_driver_interview)
+    assert i.questions.first.answers.any?
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def create_answers_and_ratings
+    Interview.all.each do |i|
+      i.questions.each do |q|
+        a = Answer.new(:candidate_id=>i.candidate_id,:interview_id=>i.id,:question_id=>q.id,response: ('a'..'z').to_a.shuffle[0,8].join)
+        if a.save
+          i.interview_participants.sample(i.interview_participants.size - 1).each do |ip|
+            a.answer_ratings.create(interviewer_id: ip.interviewer_id, rating: rand(5))
+          end
+        end
+      end
+    end
+  end
 end
