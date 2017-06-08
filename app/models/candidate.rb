97ba0a8a2dd087a71267e25e826f026949c3d224
Raviteja Pokala <ravi.ror@aol.com>
2017-06-08 16:26:12 -0400

    added a method to fetch rating of an answer

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,5 +3,10 @@ class Candidate < ApplicationRecord
   has_many :positions, through: :interviews
   has_many :questions, through: :interviews
   has_many :answers
+  has_many :answer_ratings, through: :answers
+
+  def average_rating
+    answer_ratings.where('rating != 0').average(:rating).to_f
+  end
 
 end
--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    create_answers_and_ratings
+  end
+
+  test "candidates average_rating for an answer" do
+    c = candidates(:barney_rubble)
+    ratings = c.answer_ratings.map(&:rating).reject{|r| r == 0}
+    sum = ratings.inject(:+)
+    average = (sum / ratings.size.to_f)
+    assert c.average_rating == average
+  end
+
 end
